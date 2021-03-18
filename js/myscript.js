353b8d6d1709d41fa653c623501319e20b157949
DociIva <doci.iva@hotmail.com>
2021-03-18 18:41:37 +0100

    add back

--- js/myscript.js
+++ js/myscript.js
@@ -8,17 +8,20 @@ Infine scrivi sulla pagina nomecognomecolorepreferito21 */
 console.log('Hello World')
 */
 //CHIEDI IL NOME 
-var nome = prompt('Qual è il tuo cognome?');
+var nome = prompt('Qual è il tuo nome?');
 console.log(nome);
+console.log(typeof(nome));
 
 //Chiedi il suo cognome
 var cognome = prompt('Qaul è il tuo cognome?')
 console.log(cognome);
+console.log(typeof(cognome));
 
 //Colore preferito
 var colore = prompt('Qual è il tuo colore preferito?')
 console.log(colore);
+console.log( typeof (colore));
 
 
 //infine scrivi sulla pagina nome/cognome/colore-preferito/21
-document.getElementById('title').innerHTML = ('IvaDociPetrolio21')
+document.getElementById('box-j').innerHTML = ('IvaDociPetrolio21')
